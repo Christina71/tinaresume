385c4162856f38f127a76a98ac6bcadf7f7c69c0
Christina <mariachristina71@yahoo.com>
2018-02-01 18:25:41 -0500

    Initial commit

---
+++ src/main/java/com/example/tinaresume/TinaresumeApplication.java
@@ -0,0 +1,12 @@
+package com.example.tinaresume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinaresumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinaresumeApplication.class, args);
+	}
+}
